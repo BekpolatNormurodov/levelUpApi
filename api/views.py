4252ee807bc78e60b933ccaede667a9ce23b1857
BekpolatNormurodov <bekpolatdev1995@gmail.com>
2025-03-04 21:25:51 +0500

    views

--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 
 class StoryApiView(APIView):
     def get(self, request):
-        story_1 = {"title": "Level-1", "story": ["a", "b", "c", "d"], "images": [""]}
-        story_2 = {"title": "Level-2", "story": ["aa", "ab", "ac", "ad"], "images": [""]}
+        story_1 = {"title_en": "Level-1", "title_uz": "Level-1", "story_en": ["a", "b", "c", "d"], "story_uz": ["a", "b", "c", "d"], "images": [""]}
+        story_2 = {"title_en": "Level-1", "title_uz": "Level-1", "story_en": ["a", "b", "c", "d"], "story_uz": ["a", "b", "c", "d"], "images": [""]}
 
         data = [
             story_1, 
@@ -20,7 +20,7 @@ class VocabularyApiView(APIView):
         vocabulary_2 = {"vocabulary_en": ["a", "b", "c", "d"], "vocabulary_uz": ["a", "b", "c", "d"], "sentence_en": ["a", "b", "c", "d"], "sentence_uz": ["a", "b", "c", "d"],}
 
         data = [
-            vocabulary_1, 
+            vocabulary_1,
             vocabulary_2,
         ]
         return Response(data)
